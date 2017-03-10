7cb9ca629c90c0bd474568da28ff60dd8a13acc3
Viktor Lundin <viktorlundin_@outlook.com>
2017-03-10 11:12:32 +0100

    Lagt till säker nedkoppling av judgeclienter.

--- Simhopp Backup!/WindowsFormsApplication2/GUI/AddContendersWindow.cs
+++ Simhopp Backup!/WindowsFormsApplication2/GUI/AddContendersWindow.cs
@@ -48,13 +48,7 @@ namespace WindowsFormsApplication2
                     int q = 0;
                     foreach (var x in mapObj)
                     {
-                        Hopp1TextBox.Items.Insert(q, x.Key);
-                        Hopp2TextBox.Items.Insert(q, x.Key);
-                        Hopp3TextBox.Items.Insert(q, x.Key);
-                        Hopp4TextBox.Items.Insert(q, x.Key);
-                        Hopp5TextBox.Items.Insert(q, x.Key);
-                        Hopp6TextBox.Items.Insert(q, x.Key);
-                        Hopp7TextBox.Items.Insert(q, x.Key);
+                      
                         q++;
                     }
                 }
@@ -87,7 +81,7 @@ namespace WindowsFormsApplication2
             //Kod här som slänger in informationen till ett objekt som i sin tur går till en ContestList
             //..
 
-            string line = namn_input.Text + ";" + deltagare_id_input.Text + ";" + nat_input.Text + ";" + Hopp1TextBox.Text + ";" + mapObj[Hopp1TextBox.Text] + ";" + Hopp2TextBox.Text + ";" + mapObj[Hopp2TextBox.Text] + ";" + Hopp3TextBox.Text + ";" + mapObj[Hopp3TextBox.Text] + ";" + Hopp4TextBox.Text + ";" + mapObj[Hopp4TextBox.Text] + ";" + Hopp5TextBox.Text + ";" + mapObj[Hopp5TextBox.Text] + ";" + Hopp6TextBox.Text + ";" + mapObj[Hopp6TextBox.Text] + ";" + Hopp7TextBox.Text + ";" + mapObj[Hopp7TextBox.Text];
+         //   string line = namn_input.Text + ";" + deltagare_id_input.Text + ";" + nat_input.Text + ";" + Hopp1TextBox.Text + ";" + mapObj[Hopp1TextBox.Text] + ";" + Hopp2TextBox.Text + ";" + mapObj[Hopp2TextBox.Text] + ";" + Hopp3TextBox.Text + ";" + mapObj[Hopp3TextBox.Text] + ";" + Hopp4TextBox.Text + ";" + mapObj[Hopp4TextBox.Text] + ";" + Hopp5TextBox.Text + ";" + mapObj[Hopp5TextBox.Text] + ";" + Hopp6TextBox.Text + ";" + mapObj[Hopp6TextBox.Text] + ";" + Hopp7TextBox.Text + ";" + mapObj[Hopp7TextBox.Text];
             string cName = fileNameHolder + ".txt";
             using (StreamReader sr = new StreamReader(cName))
             {
@@ -116,127 +110,122 @@ namespace WindowsFormsApplication2
             {
                 using (StreamWriter sw = File.AppendText(cName))
                 {
-                    sw.WriteLine(line);
+                    sw.WriteLine();
                 }
                 namn_input.Text = "";
                 deltagare_id_input.Text = "";
                 nat_input.Text = "";
-                Hopp1TextBox.Text = "";
-                Hopp2TextBox.Text = "";
-                Hopp3TextBox.Text = "";
-                Hopp4TextBox.Text = "";
-                Hopp5TextBox.Text = "";
-                Hopp6TextBox.Text = "";
-                Hopp7TextBox.Text = "";
+              
             }
             else { allowedID = true; }
-
-            void buttonAddAnotherContender_Click(object sender, EventArgs e)
-            {
-
-        idThatTheAdminIsTryingToGiveToTheContender = deltagare_id_input.Text;
-        if (checkConflictID.Contains(idThatTheAdminIsTryingToGiveToTheContender))
-        {
-            MessageBox.Show("ID conflict, insert a different ID.");
         }
-        else
+        void buttonAddAnotherContenders_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(namn_input.Text) || string.IsNullOrWhiteSpace(deltagare_id_input.Text) || string.IsNullOrWhiteSpace(nat_input.Text) || metroComboBox1.SelectedIndex == -1 ||
-                metroComboBox2.SelectedIndex == -1 || metroComboBox3.SelectedIndex == -1 || metroComboBox4.SelectedIndex == -1 || metroComboBox5.SelectedIndex == -1 || metroComboBox6.SelectedIndex == -1
-                || metroComboBox7.SelectedIndex == -1)
+
+            idThatTheAdminIsTryingToGiveToTheContender = deltagare_id_input.Text;
+            if (checkConflictID.Contains(idThatTheAdminIsTryingToGiveToTheContender))
             {
-                MessageBox.Show("fill all the information.");
+                MessageBox.Show("ID conflict, insert a different ID.");
             }
             else
             {
-                //Första raden!//
-                ListViewItem item = new ListViewItem();
-                item.Text = namn_input.Text; // Deltagarens namn //
-                item.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item.SubItems.Add(metroComboBox1.Text); // Lägger till vilket hopp man valt //
-                item.SubItems.Add("MaxPoints");
-                //Funktion för att läsa av combobox val (hopp) och hämta avårighetsgrad till det!
+                if (string.IsNullOrWhiteSpace(namn_input.Text) || string.IsNullOrWhiteSpace(deltagare_id_input.Text) || string.IsNullOrWhiteSpace(nat_input.Text) || metroComboBox1.SelectedIndex == -1 ||
+                    metroComboBox2.SelectedIndex == -1 || metroComboBox3.SelectedIndex == -1 || metroComboBox4.SelectedIndex == -1 || metroComboBox5.SelectedIndex == -1 || metroComboBox6.SelectedIndex == -1
+                    || metroComboBox7.SelectedIndex == -1)
+                {
+                    MessageBox.Show("fill all the information.");
+                }
+                else
+                {
+                    //Första raden!//
+                    ListViewItem item = new ListViewItem();
+                    item.Text = namn_input.Text; // Deltagarens namn //
+                    item.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item.SubItems.Add(metroComboBox1.Text); // Lägger till vilket hopp man valt //
+                    item.SubItems.Add("MaxPoints");
+                    //Funktion för att läsa av combobox val (hopp) och hämta avårighetsgrad till det!
 
-                //Rad 2 //
-                ListViewItem item1 = new ListViewItem();
-                item1.Text = namn_input.Text; // Deltagarens namn //
-                item1.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item1.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item1.SubItems.Add(metroComboBox2.Text); // Lägger till vilket hopp man valt //
-                item1.SubItems.Add("MaxPoints");
+                    //Rad 2 //
+                    ListViewItem item1 = new ListViewItem();
+                    item1.Text = namn_input.Text; // Deltagarens namn //
+                    item1.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item1.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item1.SubItems.Add(metroComboBox2.Text); // Lägger till vilket hopp man valt //
+                    item1.SubItems.Add("MaxPoints");
 
-                //Rad 3//
-                ListViewItem item3 = new ListViewItem();
-                item3.Text = namn_input.Text; // Deltagarens namn //
-                item3.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item3.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item3.SubItems.Add(metroComboBox3.Text); // Lägger till vilket hopp man valt //
-                item3.SubItems.Add("MaxPoints");
+                    //Rad 3//
+                    ListViewItem item3 = new ListViewItem();
+                    item3.Text = namn_input.Text; // Deltagarens namn //
+                    item3.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item3.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item3.SubItems.Add(metroComboBox3.Text); // Lägger till vilket hopp man valt //
+                    item3.SubItems.Add("MaxPoints");
 
-                //Rad 4//
-                ListViewItem item4 = new ListViewItem();
-                item4.Text = namn_input.Text; // Deltagarens namn //
-                item4.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item4.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item4.SubItems.Add(metroComboBox4.Text); // Lägger till vilket hopp man valt //
-                item4.SubItems.Add("MaxPoints");
+                    //Rad 4//
+                    ListViewItem item4 = new ListViewItem();
+                    item4.Text = namn_input.Text; // Deltagarens namn //
+                    item4.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item4.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item4.SubItems.Add(metroComboBox4.Text); // Lägger till vilket hopp man valt //
+                    item4.SubItems.Add("MaxPoints");
 
-                //Rad 5//
-                ListViewItem item5 = new ListViewItem();
-                item5.Text = namn_input.Text; // Deltagarens namn //
-                item5.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item5.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item5.SubItems.Add(metroComboBox5.Text); // Lägger till vilket hopp man valt //
-                item5.SubItems.Add("MaxPoints");
+                    //Rad 5//
+                    ListViewItem item5 = new ListViewItem();
+                    item5.Text = namn_input.Text; // Deltagarens namn //
+                    item5.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item5.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item5.SubItems.Add(metroComboBox5.Text); // Lägger till vilket hopp man valt //
+                    item5.SubItems.Add("MaxPoints");
 
-                //Rad 6//
-                ListViewItem item6 = new ListViewItem();
-                item6.Text = namn_input.Text; // Deltagarens namn //
-                item6.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item6.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item6.SubItems.Add(metroComboBox6.Text); // Lägger till vilket hopp man valt //
-                item6.SubItems.Add("MaxPoints");
+                    //Rad 6//
+                    ListViewItem item6 = new ListViewItem();
+                    item6.Text = namn_input.Text; // Deltagarens namn //
+                    item6.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item6.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item6.SubItems.Add(metroComboBox6.Text); // Lägger till vilket hopp man valt //
+                    item6.SubItems.Add("MaxPoints");
 
-                //Rad 7//
-                ListViewItem item7 = new ListViewItem();
-                item7.Text = namn_input.Text; // Deltagarens namn //
-                item7.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
-                item7.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
-                item7.SubItems.Add(metroComboBox7.Text); // Lägger till vilket hopp man valt //
-                item7.SubItems.Add("MaxPoints");
+                    //Rad 7//
+                    ListViewItem item7 = new ListViewItem();
+                    item7.Text = namn_input.Text; // Deltagarens namn //
+                    item7.SubItems.Add(deltagare_id_input.Text); //Lägger till deltagar ID//
+                    item7.SubItems.Add(nat_input.Text); //Lägger till nationalitet //
+                    item7.SubItems.Add(metroComboBox7.Text); // Lägger till vilket hopp man valt //
+                    item7.SubItems.Add("MaxPoints");
 
-                //Rad 8
-                ListViewItem item8 = new ListViewItem();
-                item8.Text = string.Empty;
-                item8.SubItems.Add("");
-                item8.SubItems.Add("");
-                item8.SubItems.Add("");
-                item8.SubItems.Add("");
+                    //Rad 8
+                    ListViewItem item8 = new ListViewItem();
+                    item8.Text = string.Empty;
+                    item8.SubItems.Add("");
+                    item8.SubItems.Add("");
+                    item8.SubItems.Add("");
+                    item8.SubItems.Add("");
 
-                //Lägger till alla ovan till vår listview //
-                listView1.Items.Add(item);
-                listView1.Items.Add(item1);
-                listView1.Items.Add(item3);
-                listView1.Items.Add(item4);
-                listView1.Items.Add(item5);
-                listView1.Items.Add(item6);
-                listView1.Items.Add(item7);
-                listView1.Items.Add(item8);
+                    //Lägger till alla ovan till vår listview //
+                    listView1.Items.Add(item);
+                    listView1.Items.Add(item1);
+                    listView1.Items.Add(item3);
+                    listView1.Items.Add(item4);
+                    listView1.Items.Add(item5);
+                    listView1.Items.Add(item6);
+                    listView1.Items.Add(item7);
+                    listView1.Items.Add(item8);
 
-                checkConflictID.Add(idThatTheAdminIsTryingToGiveToTheContender);
+                    checkConflictID.Add(idThatTheAdminIsTryingToGiveToTheContender);
 
-                //Tömmer alla input options //
-                namn_input.Clear();
-                deltagare_id_input.Clear();
-                nat_input.Clear();
-                metroComboBox1.SelectedIndex = -1;
-                metroComboBox2.SelectedIndex = -1;
-                metroComboBox3.SelectedIndex = -1;
-                metroComboBox4.SelectedIndex = -1;
-                metroComboBox5.SelectedIndex = -1;
-                metroComboBox6.SelectedIndex = -1;
-                metroComboBox7.SelectedIndex = -1;
+                    //Tömmer alla input options //
+                    namn_input.Clear();
+                    deltagare_id_input.Clear();
+                    nat_input.Clear();
+                    metroComboBox1.SelectedIndex = -1;
+                    metroComboBox2.SelectedIndex = -1;
+                    metroComboBox3.SelectedIndex = -1;
+                    metroComboBox4.SelectedIndex = -1;
+                    metroComboBox5.SelectedIndex = -1;
+                    metroComboBox6.SelectedIndex = -1;
+                    metroComboBox7.SelectedIndex = -1;
+                }
             }
         }
         //private void buttonAddAnotherContender_Click(object sender, EventArgs e)
